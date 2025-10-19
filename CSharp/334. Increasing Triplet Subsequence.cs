b734a7313fdb9be39dcbe5e9676d1d5c9a8ab539
artfullDev <furkan_phlvn@icloud.com>
2025-10-19 19:38:53 +0300

    .

--- CSharp/334. Increasing Triplet Subsequence.cs
+++ CSharp/334. Increasing Triplet Subsequence.cs
@@ -10,35 +10,42 @@ public class Solution
         // 2. İki değişken tutacağız:
         //    - first: şu ana kadar gördüğümüz en küçük değer
         //    - second: first’ten sonra gelen en küçük ikinci değer
-        //    Amacımız: bir üçüncü sayı bulmak (nums[k]) ki first < second < nums[k]
+        //    bir üçüncü sayı bulmak (nums[k]) ki first < second < nums[k]
         int first = int.MaxValue;
         int second = int.MaxValue;
 
         // 3. nums dizisini sırayla dolaş:
         //      - foreach ile döngüye al
-        //      - else if nums[i] <= second → second = nums[i]
-        //      - else → hem first hem second’tan büyük bir sayı bulduk → return true
         foreach (int num in nums)
         {
+            // - eğer mevcut sayı (num), first'ten küçük veya eşitse
+            // - bu sayı şu ana kadar gördüğümüz en küçük sayı olur
+            // - nums = [5, 1, 6] → ilk turda first=5, ikinci turda first=1
             if (num <= first)
             {
                 first = num;
-                // nums= [5, 1, 6]
-                // first = 5 -> sonra 1 gelir first artık 1 olur
+
             }
+
+            // - eğer num, first'ten büyük ama second'tan küçük veya eşitse
+            // - yeni bir ikinci küçük sayı bulduk
+            // - "first < second" sırasını koruyoruz
+            // - nums = [1, 3, 2] → first=1, second=2 olur
             else if (num <= second)
             {
                 second = num;
-                // nums = [1, 2, 6]
-                // first = 1, second = 2
             }
+
+            // - eğer num, hem first hem de second'dan büyükse
+            // - bu durumda artan bir üçlü bulduk: first < second < num.
+            // - nums = [1, 2, 3] bu durumda return true.
             else
             {
                 return true;
             }
         }
 
-        // 4. Döngü bittiyse → hiçbir üçlü bulunmadı → return false
+        // 4. Döngü bittiyse return false
         return false;
     }
 }
