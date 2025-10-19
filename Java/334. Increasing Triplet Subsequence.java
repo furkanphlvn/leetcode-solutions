3d3cf97ed1daf6c55215040a0b43106936f594bf
artfullDev <furkan_phlvn@icloud.com>
2025-10-19 19:39:43 +0300

    .

--- Java/334. Increasing Triplet Subsequence.java
+++ Java/334. Increasing Triplet Subsequence.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if (nums.length < 3)
-            return false;
+        if (nums.length < 3) return false;
 
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
