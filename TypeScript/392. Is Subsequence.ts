10322e0be6b4488ceb600a097bb4a04b5bf345a9
artfullDev <furkan_phlvn@icloud.com>
2025-10-22 18:32:46 +0300

    392. Is Subsequence done.

---
+++ TypeScript/392. Is Subsequence.ts
@@ -0,0 +1,14 @@
+function IsSubsequence(s: string, t: string): boolean {
+    let i = 0, j = 0;
+
+    while (i < s.length && j < t.length) {
+        if (s[i] == t[j]) i++;
+
+        j++;
+    }
+
+    if (i == s.length)
+        return true;
+    else 
+        return false;
+};
