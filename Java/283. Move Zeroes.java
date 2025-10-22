a571ab8625bb8e0bbeed5b7ce8335d781aa2c330
artfullDev <furkan_phlvn@icloud.com>
2025-10-22 15:31:59 +0300

    283 java.

---
+++ Java/283. Move Zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZero] = nums[i];
+                nonZero++;
+            }
+        }
+
+        while (nonZero < nums.length) {
+            nums[nonZero] = 0;
+            nonZero++;
+        }
+    }
+}
